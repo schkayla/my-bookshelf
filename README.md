3d203eb5f78e3341bc14822662e3352d0af8b446
schkayla <schkayla@gmail.com>
2022-04-17 10:21:24 -0700

    initial database setup

---
+++ README.md
@@ -0,0 +1,6 @@
+# [my bookshelf](https://schkayla.github.io/my-bookshelf/ "my bookshelf")
+
+Since I read often, I've been looking for a good way to store the books I've read and want to read. The plan is to use Google Sheets as a database to store my list, and be able to add/remove books from a form. I'm also going to add functionality in selecting, 'want to read' or 'have read' (if I've read the book, then I get the option to rate the book, and the thumb rating appears on the card). Later I also want to add a search feature to search by title or author, a way to filter books I've read by ones I haven't, and a filter by year the book was added. 
+
+
+`Node.js` `JavaScript` `CSS` `HTML`
---
+++ script.js
@@ -0,0 +1,109 @@
+const mobileIcon = document.querySelector('.mobile-icon');
+const sidebar = document.querySelector('.sidebar');
+const search = document.querySelector('.search');
+const searchOpen = document.querySelector('.search-open');
+const add = document.querySelector('.add');
+const addOpen = document.querySelector('.add-open');
+const remove = document.querySelector('.remove');
+const removeOpen = document.querySelector('.remove-open');
+const main = document.querySelector('.main');
+
+// Sidebar functionality desktop + mobile
+
+sidebar.addEventListener('click', () => {
+    sidebar.classList.add('sidebar-open');
+})
+
+search.addEventListener('click', () => {
+    searchOpen.classList.toggle('closed-sidebar');
+})
+
+add.addEventListener('click', () => {
+    // sidebar.classList.add('add-open');
+    addOpen.classList.toggle('closed-sidebar');
+})
+
+remove.addEventListener('click', () => {
+    // sidebar.classList.add('remove-open');
+    removeOpen.classList.toggle('closed-sidebar');
+})
+
+mobileIcon.addEventListener('click', () => {
+    sidebar.classList.remove('sidebar-close-mobile');
+})
+    
+main.addEventListener('click', () => {
+    sidebar.classList.add('sidebar-close-mobile');
+    sidebar.classList.remove('sidebar-open');
+    searchOpen.classList.add('closed-sidebar');
+    addOpen.classList.add('closed-sidebar');
+    removeOpen.classList.add('closed-sidebar');
+})
+
+// Adding card elements and info from database
+
+const grid = document.querySelector('.grid');
+
+let titleArr = [];
+let authorArr = [];
+let dateArr = [];
+let card;
+
+
+const addIcon = () => {
+    let icon = document.createElement('i')
+    icon.classList.add('material-icons-outlined');
+    icon.classList.add('icon');
+    icon.textContent = 'thumb_up';
+    card.appendChild(icon);
+}
+
+const addButton = () => {
+    let buttonDiv = document.createElement('div');
+    let yesButton = document.createElement('button');
+    let noButton = document.createElement('button');
+    buttonDiv.classList.add('toggleButton');
+    card.appendChild(buttonDiv);
+    buttonDiv.appendChild(yesButton);
+    buttonDiv.appendChild(noButton);
+    yesButton.classList.add('toggleOn')
+    yesButton.textContent = 'Have read';
+    noButton.textContent = 'Want to read';
+}
+
+fetch('https://my-bookshelf-backend.herokuapp.com/')
+    .then(response => response.json())
+    .then(data => { 
+
+        data.values.forEach(item => titleArr.push(item[0]));
+        titleArr.shift(0);
+        data.values.forEach(item => authorArr.push(item[1]));
+        authorArr.shift(0);
+        data.values.forEach(item => dateArr.push(item[2]));
+        dateArr.shift(0);
+
+        for (item in titleArr){
+            card = document.createElement('article');
+            card.classList.add('.card');
+            grid.appendChild(card);
+
+            let title = document.createElement('h3');
+            card.appendChild(title);
+            title.textContent = titleArr[item];
+
+            let author = document.createElement('p');
+            card.appendChild(author);
+            author.textContent = authorArr[item];
+
+            let date = document.createElement('p');
+            card.appendChild(date);
+            date.textContent = dateArr[item];
+
+            addIcon()
+            addButton()
+    
+        }
+        
+    })
+    .catch(e => console.log(e))
+
